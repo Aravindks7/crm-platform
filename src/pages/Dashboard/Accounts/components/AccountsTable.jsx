46ffcef876f45b80144f0472b8a7777fb5bfc342
Aravindks7 <ksaravind9@gmail.com>
2024-08-26 23:38:40 +0530

    Add Action bar and ui components

--- src/pages/Dashboard/Accounts/components/AccountsTable.jsx
+++ src/pages/Dashboard/Accounts/components/AccountsTable.jsx
@@ -3,6 +3,7 @@ import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-quartz.css";
 import { useMemo, useState } from "react";
 import { accountsData, accountColumnDefs } from "../../../../data/AccountsData";
+import AccountsActionBar from "./AccountsActionBar";
 
 const AccountsTable = () => {
   const [rowData, setRowData] = useState(accountsData);
@@ -15,17 +16,22 @@ const AccountsTable = () => {
   }, []);
 
   return (
-    <div className="m-4">
-      <div className="ag-theme-quartz" style={{ height: 500 }}>
-        <AgGridReact
-          rowData={rowData}
-          pagination={true}
-          paginationPageSize={10}
-          paginationPageSizeSelector={[10,20]}
-          rowSelection={"multiple"}
-          columnDefs={colDefs}
-          defaultColDef={defaultColDef}
-        />
+    <div>
+      <div>
+        <AccountsActionBar />
+      </div>
+      <div className="m-4">
+        <div className="ag-theme-quartz" style={{ height: 500 }}>
+          <AgGridReact
+            rowData={rowData}
+            pagination={true}
+            paginationPageSize={10}
+            paginationPageSizeSelector={[10, 20]}
+            rowSelection={"multiple"}
+            columnDefs={colDefs}
+            defaultColDef={defaultColDef}
+          />
+        </div>
       </div>
     </div>
   );
